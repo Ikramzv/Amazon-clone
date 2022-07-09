6349418320d328f673d314ece7f205945f02d7c7
Ikram <ikramzulfugar@gmail.com>
2022-07-10 00:06:06 +0400

    Cleaning directories from unnecessary things

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbtyNYDDU-6ANbpQ8M9WYaW056J4lm04Y",
+  authDomain: "clone-e3c83.firebaseapp.com",
+  projectId: "clone-e3c83",
+  storageBucket: "clone-e3c83.appspot.com",
+  messagingSenderId: "161999598470",
+  appId: "1:161999598470:web:dcfecaf83a46f90e79b207"
+};
+
+const app = initializeApp(firebaseConfig);
